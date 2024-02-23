d4e780c52fc7fd94118c51a45af77c60dc08d07c
makhammadsoliyev <makhammadsoliyev@gmail.com>
2024-02-23 22:52:35 +0500

    Add Configurations

---
+++ Configurations/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace AnjeerMarket.Configurations;
+
+public static class Constants
+{
+    public const string USER_PATH = @"..\..\..\..\AnjeerMarket\Database\users.json";
+    public const string CARTS_PATH = @"..\..\..\..\AnjeerMarket\Database\carts.json";
+    public const string ORDERS_PATH = @"..\..\..\..\AnjeerMarket\Database\orders.json";
+    public const string PRODUCTS_PATH = @"..\..\..\..\AnjeerMarket\Database\products.json";
+    public const string CART_ITEMS_PATH = @"..\..\..\..\AnjeerMarket\Database\cart_items.json";
+    public const string CATEGORIES_PATH = @"..\..\..\..\AnjeerMarket\Database\categories.json";
+    public const string ORDER_ITEMS_PATH = @"..\..\..\..\AnjeerMarket\Database\order_items.json";
+}
